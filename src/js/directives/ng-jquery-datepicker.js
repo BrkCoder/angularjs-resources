22b47ea8d5835fdca9722b7a71488fe7ba95e539
Barak Inbal <barakinbal@hotmail.com>
2015-08-01 18:52:53 +0300

    logic update
    update to ng-jquery-date-picker+boostrap

--- src/js/directives/ng-jquery-datepicker.js
+++ src/js/directives/ng-jquery-datepicker.js
@@ -13,163 +13,184 @@
  * Second we define inner scope for current directive.
  * Third we define template and link function.
  * In our current case:
- * The directive will be trigger by Attribute.
+ * The directive will be trigger by Element only.
  * The inner scope will contains the following fields:
- * 1.defaultDate : date to highlight on first opening if the field is blank
- * 2.altField and altFormat: Alternative field(input element) that is to be updated with
+ * 1.dateFormat: define the format of date output.
+ * 2.defaultDate : date to highlight on first opening if the field is blank
+ * 3.altField and altFormat: Alternative field(input element) that is to be updated with
  * the selected date from the datepicker and altFormat determine the format on which the altField will be show.
- * 3.dayNames/dayNamesMin/dayNamesShort: The list of day names in their long format , short format and minimised format.
- * 4.duration: Control the speed at which the datepicker appears, you have 3 options: "slow","normal" and "fast".
- * 5.maxDate/minDate: The maximum selectable date and the minimum selectable date.
- * 6.monthNames/monthNamesShort: The list of month names in their long format , short format and minimised format.
- * 7.numberOfMonths: The number of months to show at once.
- * 8.nextText/prevText: The text to display for the next/previous month link.
- * 9.showOn/ShowAnim: When the datepicker should appear. The datepicker can appear when the field receives focus ("focus"),
+ * 4.dayNames/dayNamesMin/dayNamesShort: The list of day names in their long format , short format and minimised format.
+ * 5.duration: Control the speed at which the datepicker appears, you have 3 options: "slow","normal" and "fast".
+ * 6.maxDate/minDate: The maximum selectable date and the minimum selectable date.
+ * 7.monthNames/monthNamesShort: The list of month names in their long format , short format and minimised format.
+ * 8.numberOfMonths: The number of months to show at once.
+ * 9.nextText: The text to display for the next/previous month link.
+ * 10.showOn/ShowAnim: When the datepicker should appear. The datepicker can appear when the field receives focus ("focus"),
  * when a button is clicked ("button"), or when either event occurs ("both")/The name of the animation used to show and hide the datepicker.
- * 10.buttonImage/buttonImageOnly/buttonText:
  * 4 watchers on the following fields: defaultDate/disabled/maxDate/minDate.
  */
 'use strict';
 angular.module('ngDatePicker',[]).
-    directive('ngJqueryDatePicker',function(){
-
+    directive('ngJqueryDatePicker',function($parse){
         return{
-            restrict: 'A',
+            restrict: "E",
             scope:{
-                id:'@',
-                class:'@',
+                id: "@",
+                class: '@',
                 title: '@',
-                state: '=',
-                defaultDate: '=',
-                disabled: '=',
-                altField: '=',
-                altFormat: '=',
-                appendText: '=',
-                autoSize: '=',
-                changeMonth: '=',
-                changeYear: '=',
-                constrainInput: '=',
-                currentText: '=',
-                dateFormat: '=',
-                dayNames: '=',
-                dayNamesMin: '=',
-                dayNamesShort: '=',
-                duration: '=',
-                firstDay: '=',
-                gotoCurrent: '=',
-                hideIfNoPrevNext: '=',
-                isRTL: '=',
-                maxDate: '=',
-                minDate: '=',
-                monthNames: '=',
-                monthNamesShort: '=',
-                navigationAsDateFormat: '=',
-                nextText: '=',
-                numberOfMonths: '=',
-                selectOtherMonths: '=',
-                shortYearCutoff: '=',
-                showAnim: '=',
-                showButtonPanel: '=',
-                showCurrentAtPos: '=',
-                showMonthAfterYear: '=',
-                showOn: '=',
-                showOtherMonths: '=',
-                showWeek: '=',
-                stepMonths: '=',
-                weekHeader: '=',
-                yearRange: '=',
-                yearSuffix: '=',
-                onSelect: '=',
-                onOpen: '=',
-                onClose: '='
-
-
+                dateFormat: '@',
+                defaultDate: '@',
+                altField: '@',
+                altFormat: '@',
+                dayNames: '@',
+                dayNamesMin: '@',
+                dayNamesShort: '@',
+                duration: '@',
+                maxDate: '@',
+                minDate: '@',
+                monthNames: '@',
+                monthNamesShort: '@',
+                numberOfMonths: '@',
+                showOn: '@',
+                showAnim: '@',
+                disabled: '@',
+                appendText: '@',
+                autoSize: '@',
+                changeMonth: '@',
+                changeYear: '@',
+                constrainInput: '@',
+                currentText: '@',
+                firstDay: '@',
+                gotoCurrent: '@',
+                hideIfNoPrevNext: '@',
+                isRTL: '@',
+                navigationAsDateFormat: '@',
+                nextText: '@',
+                selectOtherMonths: '@',
+                shortYearCutoff: '@',
+                showButtonPanel: '@',
+                showCurrentAtPos: '@',
+                showMonthAfterYear: '@',
+                showOtherMonths: '@',
+                showWeek: '@',
+                stepMonths: '@',
+                weekHeader: '@',
+                yearRange: '@',
+                yearSuffix: '@'
             },
-            require: 'ngModel',
-            link: function (scope, element, attrs, ngModelCtrl) {
-                var watchers = [];
-                var options = {
-                    dateFormat: scope.dateFormat || 'dd.mm.yy',
-                    defaultDate: scope.defaultDate || null,
-                    disabled: scope.disabled || false,
-                    altField: scope.altField || "",
-                    altFormat: scope.altFormat || "",
-                    appendText: scope.appendText || "",
-                    autoSize: scope.autoSize || false,
-                    changeMonth: scope.changeMonth || false,
-                    changeYear: scope.changeYear || false,
-                    constrainInput: scope.constrainInput || true,
-                    currentText: scope.currentText || "Today",
-                    dayNames: scope.dayNames || [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ],
-                    dayNamesMin: scope.dayNamesMin || [ "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" ],
-                    dayNamesShort: scope.dayNamesShort || [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ],
-                    duration: scope.duration || "normal",
-                    firstDay: scope.firstDay || 0,
-                    gotoCurrent: scope.gotoCurrent || false,
-                    hideIfNoPrevNext: scope.hideIfNoPrevNext || false,
-                    isRTL: scope.isRTL || false,
-                    maxDate: scope.maxDate || null,
-                    minDate: scope.minDate || null,
-                    monthNames: scope.monthNames || [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
-                    monthNamesShort: scope.monthNamesShort || [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
-                    navigationAsDateFormat: scope.navigationAsDateFormat || false,
-                    nextText: scope.nextText || "Next",
-                    numberOfMonths: scope.numberOfMonths || 1,
-                    selectOtherMonths: scope.selectOtherMonths || false,
-                    shortYearCutoff: scope.shortYearCutoff || "+10",
-                    showAnim: scope.showAnim || "show",
-                    showButtonPanel: scope.showButtonPanel || false,
-                    showCurrentAtPos: scope.showCurrentAtPos || 0,
-                    showMonthAfterYear: scope.showMonthAfterYear || false,
-                    showOn: scope.showOn || "focus",
-                    showOtherMonths: scope.showOtherMonths || false,
-                    showWeek: scope.showWeek || false,
-                    stepMonths: scope.stepMonths || 1,
-                    weekHeader: scope.weekHeader || "Wk",
-                    yearRange: scope.yearRange || "c-10:c+10",
-                    yearSuffix: scope.yearSuffix || "",
-                    onSelect:function (date) {
-                        if(scope.onSelect && typeof scope.onSelect == "function"){
-                            ngModelCtrl.$setViewValue(date);
-                            scope.$apply();
-                        }
+            replace:true,
+            transclude: false,
+            //define the compile function
+            compile: function (element, attrs) {
+
+                var ngModelAttr = $parse(attrs.ngModel);
+                var template = "<input type='text' id='" + attrs.id + 'ng-jquery-date-picker'+ "'"+
+                                " class="+attrs.class+" >" +"</input>";
+                element.replaceWith($(template));
+
+                //return a closure
+                return function(scope, element, attrs) {
+
+                    var updateProcedure = function () {
+
+                        scope.$apply(function (scope) {
+                            ngModelAttr.assign(scope, new Date(element.datepicker("getDate")));
+                        });
+                    };
+
+                    var elements2Array = function (str) {
+                        return str.split(',');
                     }
+                    console.log('scope.dayNamesMin',scope.dayNamesMin);
+                    //define datepicker options
+                    var options = {
+                        inline: true,
+                        dateFormat: scope.dateFormat || 'dd.mm.yy',
+                        defaultDate: scope.defaultDate || null,
+                        altField: scope.altField || "",
+                        altFormat: scope.altFormat || "" ,
+                        dayNames:(typeof(scope.dayNames) !==  "undefined") ? elements2Array(scope.dayNames) :
+                                        [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ],
+                        dayNamesShort: (typeof(scope.dayNamesShort) !==  "undefined") ? elements2Array(scope.dayNamesShort) :
+                                        [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ],
+                        dayNamesMin: (typeof(scope.dayNamesMin) !==  "undefined") ? elements2Array(scope.dayNamesMin) :
+                                        [ "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" ] ,
+                        duration: scope.duration || "normal",
+                        maxDate: scope.maxDate || null,
+                        minDate: scope.minDate || null,
+                        monthNames: (typeof(scope.monthNames)  !==  "undefined") ? elements2Array(scope.monthNames) :
+                                         [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
+                        monthNamesShort: (typeof(scope.monthNamesShort) !==  "undefined") ? elements2Array(scope.monthNamesShort) :
+                                        [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
+                        numberOfMonths: scope.numberOfMonths || 1,
+                        showOn: scope.showOn || "focus",
+                        showAnim: scope.showAnim || "show",
+                        disabled: scope.disabled || false,
+                        appendText: scope.appendText || "",
+                        autoSize: scope.autoSize || false,
+                        changeMonth: scope.changeMonth || false,
+                        changeYear: scope.changeYear || false,
+                        constrainInput: scope.constrainInput || true,
+                        currentText: scope.currentText || "Today",
+                        firstDay: scope.firstDay || 0,
+                        gotoCurrent: scope.gotoCurrent || false,
+                        hideIfNoPrevNext: scope.hideIfNoPrevNext || false,
+                        isRTL: scope.isRTL || false,
+                        navigationAsDateFormat: scope.navigationAsDateFormat || false,
+                        nextText: '@',
+                        selectOtherMonths: scope.selectOtherMonths || false,
+                        shortYearCutoff: scope.shortYearCutoff || "+10",
+                        showButtonPanel: scope.showButtonPanel || false,
+                        showCurrentAtPos: scope.showCurrentAtPos || 0,
+                        showMonthAfterYear: scope.showMonthAfterYear || false,
+                        showOtherMonths: scope.showOtherMonths || false,
+                        showWeek: scope.showWeek || false,
+                        stepMonths: scope.stepMonths || 1,
+                        weekHeader: scope.weekHeader || "Wk",
+                        yearRange: scope.yearRange || "c-10:c+10",
+                        yearSuffix: scope.yearSuffix || "",
+                        onClose: updateProcedure,
+                    }
+
+                    //init jquery ui datepicker object
+                    element.datepicker(options);
+
                 };
 
-                 //Initialize jquery date picker
-                 element.datepicker(options);
+                //scope.$watch(modelAccessor, function (val) {
+                //    var date = new Date(val);
+                //    element.datepicker("setDate", date);
+                //});
 
-                 //Define watchers
-                 var initWatchers = scope.$watch( "ready", function(){
-                     watchers.push( scope.$watch( "defaultDate", function(){
-                            console.log('defaultDate',scope.defaultDate);
-                            element.datepicker( "option", "defaultDate", scope.defaultDate );
-
-                     }));
-
-                     watchers.push( scope.$watch( "disabled", function(){
-                            console.log('disabled',scope.disabled);
-                            element.datepicker("option","disabled",false);
-                            element.datepicker( "refresh" );
-                     }));
-
-                     watchers.push( scope.$watch( "maxDate", function(){
-                            console.log('maxDate',scope.maxDate);
-                            element.datepicker( "option", "maxDate", scope.maxDate );
-                         element.datepicker( "refresh" );
-                     }));
-
-                     watchers.push( scope.$watch( "minDate", function(){
-
-                            element.datepicker( "option", "minDate", scope.minDate );
-                            element.datepicker( "refresh" );
-
-                     }));
-
-                 });
-                initWatchers();
-
+                //Define watchers
+                //var initWatchers = scope.$watch( "ready", function(){
+                //    watchers.push( scope.$watch( "defaultDate", function(){
+                //        console.log('defaultDate',scope.defaultDate);
+                //        element.datepicker( "option", "defaultDate", scope.defaultDate );
+                //
+                //    }));
+                //
+                //    watchers.push( scope.$watch( "disabled", function(){
+                //        console.log('disabled',scope.disabled);
+                //        element.datepicker("option","disabled",false);
+                //        element.datepicker( "refresh" );
+                //    }));
+                //
+                //    watchers.push( scope.$watch( "maxDate", function(){
+                //        console.log('maxDate',scope.maxDate);
+                //        element.datepicker( "option", "maxDate", scope.maxDate );
+                //        element.datepicker( "refresh" );
+                //    }));
+                //
+                //    watchers.push( scope.$watch( "minDate", function(){
+                //
+                //        element.datepicker( "option", "minDate", scope.minDate );
+                //        element.datepicker( "refresh" );
+                //
+                //    }));
+                //
+                //});
+                //initWatchers();
             }
-
         }
-});
+    })
